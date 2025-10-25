49c680ac3d17a65e56392bce1076e7afd82335b0
salem <salembakhouche42@gmail.com>
2025-10-25 18:37:59 +0100

    added the db infos

--- config/database.php
+++ config/database.php
@@ -1,25 +1,24 @@
 <?php
 
-
 if (in_array($_SERVER['HTTP_HOST'], ['localhost', '127.0.0.1'])) {
+    // Local XAMPP setup
     $host = "localhost";
     $username = "root";
     $password = "";
+    $dbname = "nextgen_website";
 } else {
-    $host = "your_remote_host";
-    $username = "your_remote_username";
-    $password = "your_remote_password";
+    // Remote FreeSQLDatabase connection
+    $host = "sql8.freesqldatabase.com";
+    $username = "sql8804504";
+    $password = "PtpePIsLqq";
+    $dbname = "sql8804504";
 }
 
-$dbname = "nextgen_website";
-
-
 $conn = new mysqli($host, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
 $conn->set_charset("utf8");
+?>
