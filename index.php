831f92c5a4b3d1befbb1cd1a0a8f0b970195dd75
salem <salembakhouche42@gmail.com>
2025-10-29 13:38:02 +0100

    google tag manager

--- index.php
+++ index.php
@@ -26,6 +26,13 @@ while ($row = $result->fetch_assoc()) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KTLX7DTM');</script>
+<!-- End Google Tag Manager -->
     <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-FE5LRLL2MK"></script>
 <script>
@@ -562,6 +569,10 @@ while ($row = $result->fetch_assoc()) {
     
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KTLX7DTM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     
     <?php include 'includes/navbar.php'; ?>
     <?php include 'includes/access-modal.php'; ?>
